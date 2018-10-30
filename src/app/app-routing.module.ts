6827829db84b8281e707a1df6fe54d59d5c3ba5d
webelf000 <steven.jung86000@gmail.com>
2018-10-30 09:18:00 +0100

    adding update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {CustomersListComponent} from './customers/customers-list/customers-list.component';
 import {CustomerDetailsComponent} from './customers/customer-details/customer-details.component';
 import {CustomerAddComponent} from './customers/customer-add/customer-add.component';
+import {CustomerUpdateComponent} from './customers/customer-update/customer-update.component';
 
 const routes: Routes = [
   { path: 'customers/:id', component: CustomerDetailsComponent },
+  { path: 'customer-update/:id', component: CustomerUpdateComponent },
   { path: 'customer-add', component: CustomerAddComponent },
   { path: 'customers', component: CustomersListComponent },
   { path: '', component: WelcomeComponent }
