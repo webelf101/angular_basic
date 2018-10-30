9152304c2876a0fb6898d63b7ff9f96ce652e3b7
webelf000 <steven.jung86000@gmail.com>
2018-10-30 09:40:11 +0100

    Adding update functionality

--- src/app/customers/customer-update/customer-update.component.ts
+++ src/app/customers/customer-update/customer-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CustomerService} from '../../shared/services/customer.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-customer-update',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-update.component.css']
 })
 export class CustomerUpdateComponent implements OnInit {
+  customerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private customerService: CustomerService,
+              private router: Router) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    const customer = this.customerService.getCustomerById(id);
+    this.customerForm.patchValue({
+      firstName: customer.firstName,
+      lastName: customer.lastName,
+      address: customer.address
+    });
   }
+
+  save() {
+    /*const customer = this.customerForm.value;
+    this.customerService.updateCustomer(customer);*/
+  }
+
 
 }
